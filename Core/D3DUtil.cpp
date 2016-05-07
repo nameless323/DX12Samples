f6ff42ce727f2f3311ad379418af9f8747d1f28a
nameless <nameless323@gmail.com>
2016-05-07 19:05:52 +0300

    d3d util

---
+++ Core/D3DUtil.cpp
@@ -0,0 +1,79 @@
+#include "D3DUtil.h"
+#include <comdef.h>
+#include <fstream>
+
+using Microsoft::WRL::ComPtr;
+
+DxException::DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& filename, int lineNumber) :
+    ErrorCode(hr), FunctionName(functionName), FileName(filename), LineNumber(lineNumber)
+{}
+
+bool D3DUtil::IsKeyDown(int keyCode)
+{
+    return (GetAsyncKeyState(keyCode) & 0x8000) != 0;
+}
+
+ComPtr<ID3DBlob> D3DUtil::LoadBinary(const std::wstring& filename)
+{
+    std::ifstream fin(filename, std::ios::binary);
+    fin.seekg(0, std::ios_base::end);
+    std::ifstream::pos_type size = (int)fin.tellg();
+    fin.seekg(0, std::ios_base::beg);
+
+    ComPtr<ID3DBlob> blob;
+    ThrowIfFailed(D3DCreateBlob(size, blob.GetAddressOf()));
+
+    fin.read((char*)blob->GetBufferPointer(), size);
+    fin.close();
+    return blob;
+}
+
+ComPtr<ID3D12Resource> D3DUtil::CreateDefaultBuffer(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, const void* initData, UINT64 byteSize, Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer)
+{
+    ComPtr<ID3D12Resource> defaultBuffer;
+    ThrowIfFailed(device->CreateCommittedResource
+        (
+            &CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT),
+            D3D12_HEAP_FLAG_NONE,
+            &CD3DX12_RESOURCE_DESC::Buffer(byteSize),
+            D3D12_RESOURCE_STATE_COMMON,
+            nullptr,
+            IID_PPV_ARGS(defaultBuffer.GetAddressOf())
+        ));
+
+    D3D12_SUBRESOURCE_DATA subResourceData = {};
+    subResourceData.pData = initData;
+    subResourceData.RowPitch = byteSize;
+    subResourceData.SlicePitch = subResourceData.RowPitch;
+
+    cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(defaultBuffer.Get(), D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST));
+    UpdateSubresources<1>(cmdList, defaultBuffer.Get(), uploadBuffer.Get(), 0, 0, 1, &subResourceData);
+    cmdList->ResourceBarrier(1, &CD3DX12_RESOURCE_BARRIER::Transition(defaultBuffer.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_GENERIC_READ));
+    return defaultBuffer;
+}
+
+ComPtr<ID3DBlob> D3DUtil::CompileShader(const std::wstring& fileName, const D3D_SHADER_MACRO* defines, const std::string& entryPoint, const std::string& target)
+{
+    UINT compileFlags = 0;
+#if defined(DEBUG) || defined(_DEBUG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    HRESULT hr = S_OK;
+    ComPtr<ID3DBlob> byteCode = nullptr;
+    ComPtr<ID3DBlob> errors;
+    hr = D3DCompileFromFile(fileName.c_str(), defines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+        entryPoint.c_str(), target.c_str(), compileFlags, 0, &byteCode, &errors);
+
+    if (errors != nullptr)
+        OutputDebugStringA((char*)errors->GetBufferPointer());
+    ThrowIfFailed(hr);
+    return byteCode;
+}
+
+std::wstring DxException::ToString() const
+{
+    _com_error err(ErrorCode);
+    std::wstring msg = err.ErrorMessage();
+    return FunctionName + L" failed in " + FileName + L"; Line" + std::to_wstring(LineNumber) + L"; error" + msg;
+}
---
+++ Core/D3DUtil.h
@@ -0,0 +1,194 @@
+#pragma once
+
+#include <windows.h>
+#include <wrl.h>
+#include <dxgi1_4.h>
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+#include <array>
+#include <unordered_map>
+#include <cstdint>
+#include <fstream>
+#include <sstream>
+#include <cassert>
+#include "d3dx12.h"
+#include "DDSTextureLoader.h"
+#include "MathHelper.h"
+
+extern const int gNumFrameResources;
+
+inline void D3DSetDebugName(IDXGIObject* obj, const char* name)
+{
+    if (obj != nullptr)
+        obj->SetPrivateData(WKPDID_D3DDebugObjectName, lstrlenA(name), name);
+}
+
+inline void D3DSetDebugName(ID3D12Device* obj, const char* name)
+{
+    if (obj != nullptr)
+        obj->SetPrivateData(WKPDID_D3DDebugObjectName, lstrlenA(name), name);
+}
+
+inline void D3DSetDebugName(ID3D12DeviceChild* obj, const char* name)
+{
+    if (obj != nullptr)
+        obj->SetPrivateData(WKPDID_D3DDebugObjectName, lstrlenA(name), name);
+}
+
+inline std::wstring AnsiToWString(const std::string& str)
+{
+    WCHAR buffer[512];
+    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
+    return std::wstring(buffer);
+}
+
+class D3DUtil
+{
+public:
+    static bool IsKeyDown(int keyCode);
+    static std::string ToString(HRESULT hr);
+    static UINT CalcConstantBufferByteSize(UINT byteSize)
+    {
+        return (byteSize + 255) & ~255;
+    }
+
+    static Microsoft::WRL::ComPtr<ID3DBlob> LoadBinary(const std::wstring& filename);
+
+    static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer
+        (
+            ID3D12Device* device, ID3D12GraphicsCommandList* cmdList,
+            const void* initData, UINT64 byteSize,
+            Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer
+        );
+    static Microsoft::WRL::ComPtr<ID3DBlob> CompileShader
+        (
+            const std::wstring& fileName,
+            const D3D_SHADER_MACRO* defines,
+            const std::string& entryPoint,
+            const std::string& target
+        );
+};
+
+class DxException
+{
+public:
+    DxException() = default;
+    DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& filename, int lineNumber);
+
+    std::wstring ToString() const;
+
+    HRESULT ErrorCode = S_OK;
+    std::wstring FunctionName;
+    std::wstring FileName;
+    int LineNumber = -1;
+};
+
+struct SubmeshGeometry
+{
+    UINT IndexCount = 0;
+    UINT StartIndexLocation = 0;
+    INT BaseVertexLocation = 0;
+
+    DirectX::BoundingBox Bounds;
+};
+
+struct MeshGeometry
+{
+    std::string Name;
+
+    Microsoft::WRL::ComPtr<ID3DBlob> VertexBufferCPU = nullptr;
+    Microsoft::WRL::ComPtr<ID3DBlob> IndexBufferCPU = nullptr;
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferGPU = nullptr;
+    Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferGPU = nullptr;
+
+    Microsoft::WRL::ComPtr<ID3D12Resource> VertexBufferUploader = nullptr;
+    Microsoft::WRL::ComPtr<ID3D12Resource> IndexBufferUploader = nullptr;
+
+    UINT VertexByteStride = 0;
+    UINT VertexBufferByteSize = 0;
+    DXGI_FORMAT IndexFormat = DXGI_FORMAT_R16_UINT;
+    UINT IndexBufferByteSize = 0;
+
+    std::unordered_map<std::string, SubmeshGeometry> DrawArgs;
+
+    D3D12_VERTEX_BUFFER_VIEW VertexBufferView() const
+    {
+        D3D12_VERTEX_BUFFER_VIEW vbv;
+        vbv.BufferLocation = VertexBufferGPU->GetGPUVirtualAddress();
+        vbv.StrideInBytes = VertexByteStride;
+        vbv.SizeInBytes = VertexBufferByteSize;
+        return vbv;
+    }
+
+    D3D12_INDEX_BUFFER_VIEW IndexBufferView() const
+    {
+        D3D12_INDEX_BUFFER_VIEW ibv;
+        ibv.BufferLocation = IndexBufferGPU->GetGPUVirtualAddress();
+        ibv.Format = IndexFormat;
+        ibv.SizeInBytes = IndexBufferByteSize;
+        return ibv;
+    }
+
+    void DisposeUploaders()
+    {
+        VertexBufferUploader = nullptr;
+        IndexBufferUploader = nullptr;
+    }
+};
+
+struct Light
+{
+    DirectX::XMFLOAT3 Strength = { 0.5f, 0.5f, 0.5f };
+    float FalloffStart = 1.0f;
+    DirectX::XMFLOAT3 Direction = { 0.0f, -1.0f, 0.0f };
+    float FalloffEnd = 10.0f;
+    DirectX::XMFLOAT3 Position = { 0.0f, 0.0f, 0.0f };
+    float SpotPower = 64.0f;
+};
+
+#define MaxLights 16
+
+struct MaterialConstants
+{
+    DirectX::XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
+    DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
+    float Roughness = 0.25f;
+
+    DirectX::XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
+};
+
+struct Material
+{
+    std::string Name;
+    int MatCBIndex = -1;
+    int DiffuseSrvHeapIndex = -1;
+    int NormalSrvHeapIndex = -1;
+    int NumFramesDirty = gNumFrameResources;
+
+    DirectX::XMFLOAT4 DiffuseAlbedo = { 1.0f, 1.0f, 1.0f, 1.0f };
+    DirectX::XMFLOAT3 FresnelR0 = { 0.01f, 0.01f, 0.01f };
+    float Roughness = 0.25f;
+    DirectX::XMFLOAT4X4 MatTransform = MathHelper::Identity4x4();
+};
+
+#ifndef ThrowIfFailed
+#define ThrowIfFailed(x)                                              \
+{                                                                     \
+    HRESULT hr__ = (x);                                               \
+    std::wstring wfn = AnsiToWString(__FILE__);                       \
+    if (FAILED(hr__)) { throw DxException(hr__, L#x, wfn, __LINE__); }\
+}
+#endif
+
+#ifndef ReleaseCom
+#define ReleaseCom(x) { if (x) { x->Release(); x = 0; }}
+#endif
