e522ef45d90cb266a8a1798d744b134c7ffe77d1
nameless <nameless323@gmail.com>
2016-07-21 10:20:06 +0300

    GaussBlur but need debuging

---
+++ Shaders/GaussBlur.hlsl
@@ -0,0 +1,85 @@
+cbuffer cbSettings : register(b0)
+{
+    int BlurRadius;
+
+    float w0;
+    float w1;
+    float w2;
+    float w3;
+    float w4;
+    float w5;
+    float w6;
+    float w7;
+    float w8;
+    float w9;
+    float w10;
+};
+
+static const int _maxBlurRadius = 5;
+
+Texture2D _input : register(t0);
+RWTexture2D<float4> _output : register(u0);
+
+#define N 256
+#define CacheSize (N + 2 * _maxBlurRadius)
+groupshared float4 _cache[CacheSize];
+
+[numthreads(N, 1, 1)]
+void HorizBlurCS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
+{
+    float weights[11] = { w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
+
+    if (groupThreadID.x < BlurRadius)
+    {
+        int x = max(dispatchThreadID.x - BlurRadius, 0);
+        _cache[groupThreadID.x] = _input[int2(x, dispatchThreadID.y)];
+    }
+    if (groupThreadID.x >= N - BlurRadius)
+    {
+        int x = min(dispatchThreadID.x + BlurRadius, _input.Length.x - 1);
+        _cache[groupThreadID.x + 2 * BlurRadius] = _input[int(x, dispatchThreadID.y)];
+    }
+
+    _cache[groupThreadID.x + BlurRadius] = _input[min(dispatchThreadID.xy, _input.Length.xy - 1)];
+
+    GroupMemoryBarrierWithGroupSync();
+
+    float4 blurColor = float4(0, 0, 0, 0);
+
+    for (int i = -BlurRadius; i <= BlurRadius; i++)
+    {
+        int k = groupThreadID.x + BlurRadius + i;
+        blurColor += weights[i + BlurRadius] * _cache[k];
+    }
+    _output[dispatchThreadID.xy] = blurColor;
+}
+
+[numthreads(1, N, 1)]
+void VertBlurCS(int3 groupThreadID : SV_GroupThreadID, int3 dispatchThreadID : SV_DispatchThreadID)
+{
+    float weights[11] = { w0, w1, w2, w3, w4, w5, w6, w7, w8, w9, w10 };
+
+    if (groupThreadID.y < BlurRadius)
+    {
+        int y = max(dispatchThreadID.y - BlurRadius, 0);
+        _cache[groupThreadID.y] = _input[int2(dispatchThreadID.x, y)];
+    }
+    if (groupThreadID.y >= N - BlurRadius)
+    {
+        int x = min(dispatchThreadID.y + BlurRadius, _input.Length.y - 1);
+        _cache[groupThreadID.y + 2 * BlurRadius] = _input[int(dispatchThreadID.x, y)];
+    }
+
+    _cache[groupThreadID.y + BlurRadius] = _input[min(dispatchThreadID.xy, _input.Length.xy - 1)];
+
+    GroupMemoryBarrierWithGroupSync();
+
+    float4 blurColor = float4(0, 0, 0, 0);
+
+    for (int i = -BlurRadius; i <= BlurRadius; i++)
+    {
+        int k = groupThreadID.y + BlurRadius + i;
+        blurColor += weights[i + BlurRadius] * _cache[k];
+    }
+    _output[dispatchThreadID.xy] = blurColor;
+}
---
+++ Source/Scenes/GaussBlur/GaussBlur.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include "../../../Core/Application.h"
+#include "../../../Core/D3DUtil.h"
+
+#include "../../Common/RenderItem.h"
+#include "../Waves/Waves.h"
+#include "../../Common/FrameResource.h"
+#include "GaussBlurFilter.h"
+
+class GaussBlur : public Application
+{
+public:
+    GaussBlur(HINSTANCE hInstance);
+    GaussBlur(const GaussBlur& rhs) = delete;
+    GaussBlur& operator=(const GaussBlur& rhs) = delete;
+    ~GaussBlur() override;
+
+    bool Init() override;
+    LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
+    int Run() override;
+protected:
+    void OnResize() override;
+    void Update(const GameTimer& timer) override;
+    void Draw(const GameTimer& timer) override;
+
+    void OnMouseDown(WPARAM btnState, int x, int y) override;
+    void OnMouseUp(WPARAM btnState, int x, int y) override;
+    void OnMouseMove(WPARAM btnState, int x, int y) override;
+
+    void OnKeyboardInput(const GameTimer& timer);
+    void UpdateCamera(const GameTimer& timer);
+    void AnimateMaterials(const GameTimer& timer);
+    void UpdateObjectCBs(const GameTimer& timer);
+    void UpdateMaterialCBs(const GameTimer& timer);
+    void UpdateMainPassCB(const GameTimer& timer);
+    void UpdateWaves(const GameTimer& timer);
+
+    void LoadTextures();
+    void BuildRootSignature();
+    void BuildPostProcessRootSignature();
+    void BuildDescriptorHeaps();
+    void BuildShaderAndInputLayout();
+    void BuildLandGeometry();
+    void BuildWavesGeometry();
+    void BuildBoxGeometry();
+    void BuildPSOs();
+    void BuildFrameResources();
+    void BuildMaterials();
+    void BuildRenderItems();
+    void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& renderItems);
+
+    float GetHillsHeight(float x, float z) const;
+    DirectX::XMFLOAT3 GetHillsNormal(float x, float z) const;
+private:
+    std::vector<std::unique_ptr<FrameResource>> _frameResources;
+    FrameResource* _currFrameResource = nullptr;
+    int _currentFrameResourceIndex = 0;
+
+    Microsoft::WRL::ComPtr<ID3D12RootSignature> _rootSignature = nullptr;
+    Microsoft::WRL::ComPtr<ID3D12RootSignature> _postProcessRootSignature = nullptr;
+
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> _srvHeap = nullptr;
+
+    std::unordered_map<std::string, std::unique_ptr<MeshGeometry>> _geometries;
+    std::unordered_map<std::string, std::unique_ptr<Material>> _materials;
+    std::unordered_map<std::string, std::unique_ptr<Texture>> _textures;
+    std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3DBlob>> _shaders;
+    std::unordered_map<std::string, Microsoft::WRL::ComPtr<ID3D12PipelineState>> _PSOs;
+
+    std::vector<D3D12_INPUT_ELEMENT_DESC> _inputLayout;
+    RenderItem* _wavesRenderItem = nullptr;
+
+    std::vector<std::unique_ptr<RenderItem>> _allRenderItems;
+    std::vector<RenderItem*> _renderItemLayer[(int)RenderItem::RenderLayer::Count];
+    std::unique_ptr<Waves> _waves;
+
+    FrameResource::PassConstants _mainPassCB;
+    std::unique_ptr<GaussBlurFilter> _blurFilter;
+
+    bool _isWireframe = false;
+
+    DirectX::XMFLOAT3 _eyePos = { 0.0f, 0.0f, 0.0f };
+    DirectX::XMFLOAT4X4 _view = MathHelper::Identity4x4();
+    DirectX::XMFLOAT4X4 _proj = MathHelper::Identity4x4();
+
+    float _theta = 1.5f * DirectX::XM_PI;
+    float _phi = DirectX::XM_PIDIV2 - 0.1f;
+    float _radius = 50.0f;
+
+    float _sunTheta = 1.25f * DirectX::XM_PI;
+    float _sunPhi = DirectX::XM_PIDIV4;
+
+    POINT _lastMousePos;
+};
