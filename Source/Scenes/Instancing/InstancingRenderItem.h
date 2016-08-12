d7102c7c070f5db7db82bbf5a106139314455686
nameless <nameless323@gmail.com>
2016-08-12 23:46:56 +0300

    instancing frame resource and render item

---
+++ Source/Scenes/Instancing/InstancingRenderItem.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "../../Core/D3DUtil.h"
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include "InstancingFrameResource.h"
+
+struct RenderItem
+{
+    RenderItem() = default;
+    RenderItem(const RenderItem& rhs) = delete;
+
+    DirectX::XMFLOAT4X4 Model = MathHelper::Identity4x4();
+    DirectX::XMFLOAT4X4 TexTransform = MathHelper::Identity4x4();
+
+    int NumFramesDirty = 3;
+
+    UINT ObjCBIndex = -1;
+    Material* Mat = nullptr;
+    MeshGeometry* Geo = nullptr;
+
+    D3D12_PRIMITIVE_TOPOLOGY PrimitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+    DirectX::BoundingBox Bounds;
+    std::vector<InstancingFrameResource::InstanceData> Instances;
+
+    UINT IndexCount = 0;
+    UINT InstanceCount = 0;
+    UINT StartIndexLocation = 0;
+    UINT BaseVertexLocation = 0;
+};
