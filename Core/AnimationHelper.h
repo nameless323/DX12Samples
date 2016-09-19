d102010fde0812824ba0eefe3d4aa49f2e1f80f8
nameless <nameless323@gmail.com>
2016-09-19 23:53:12 +0300

    animation helper

---
+++ Core/AnimationHelper.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "D3DUtil.h"
+
+struct Keyframe
+{
+    Keyframe();
+    ~Keyframe();
+
+    float TimePos;
+    DirectX::XMFLOAT3 Translation;
+    DirectX::XMFLOAT3 Scale;
+    DirectX::XMFLOAT4 RotationQuat;
+};
+
+struct BoneAnimation
+{
+    float GetStartTime() const;
+    float GetEndTime() const;
+
+    void Interpolate(float t, DirectX::XMFLOAT4X4& M) const;
+
+    std::vector<Keyframe> Keyframes;
+};
