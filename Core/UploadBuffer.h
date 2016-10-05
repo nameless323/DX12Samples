1c68c547142237f3522eeb577240151aeeb35aad
Alex Vorontsov <nameless323@gmail.com>
2016-10-05 17:23:11 +0400

    Upload buffer comments

--- Core/UploadBuffer.h
+++ Core/UploadBuffer.h
@@ -1,10 +1,21 @@
+//
+// Simple class which helps to create constant buffers.
+//
+
 #pragma once
+
 #include "D3DUtil.h"
 
 template<typename T>
 class UploadBuffer
 {
 public:
+    /**
+     * \brief Creates upload buffer.
+     * \param device to create resource.
+     * \param elementCount how much elements of type T will be in buffer.
+     * \param isConstantBuffer if this buffer are constant buffer then size of each element must be multiple of 256 bytes.
+     */
     UploadBuffer(ID3D12Device* device, UINT elementCount, bool isConstantBuffer) : _isConstantBuffer(isConstantBuffer)
     {
         _elementByteSize = sizeof(T);
@@ -24,12 +35,16 @@ public:
             _uploadBuffer->Unmap(0, nullptr);
         _mappedData = nullptr;
     }
-
+    /**
+     * \brief Get constant buffer resource.
+     */
     ID3D12Resource* Resource() const
     {
         return _uploadBuffer.Get();
     }
-
+    /**
+     * \brief Overwrite data at constant buffer for element with index elementIndex to data.
+     */
     void CopyData(int elementIndex, const T& data)
     {
         memcpy(&_mappedData[elementIndex*_elementByteSize], &data, sizeof(T));
