1d73245bbc99b1ccbdadc7b8b99fd1c045bdf898
Alex Vorontsov <nameless323@gmail.com>
2016-10-06 15:23:07 +0400

    GeometryGenerator doc comments

--- Core/GeometryGenerator.h
+++ Core/GeometryGenerator.h
@@ -1,3 +1,7 @@
+//
+// Helper class for different procedural generated meshes.
+//
+
 #pragma once
 
 #include <cstdint>
@@ -9,7 +13,9 @@ class GeometryGenerator
 public:
     using uint16 = uint16_t;
     using uint32 = uint32_t;
-
+    /**
+     * \brief Struct which describes single vertex.
+     */
     struct Vertex
     {
         Vertex() {}
@@ -28,7 +34,10 @@ public:
         DirectX::XMFLOAT3 Tangent;
         DirectX::XMFLOAT2 TexCoord;
     };
-
+    
+     /**
+     * \brief Struct which describes mesh data (indices and vertices).
+     */
     struct MeshData
     {
         std::vector<Vertex> Vertices;
@@ -49,40 +58,35 @@ public:
     private:
         std::vector<uint16> _indices16;
     };
-    ///<summary>
-    /// Creates a box centered at the origin with the given dimensions, where each
-    /// face has m rows and n columns of vertices.
-    ///</summary>
+     /**
+     * \brief Struct which describes single vertex. Creates a box centered at the origin with the given dimensions, where each
+     * face has m rows and n columns of vertices.
+     */
     MeshData CreateBox(float width, float height, float depth, uint32 numSubdivisions);
-
-    ///<summary>
-    /// Creates a sphere centered at the origin with the given radius.  The
-    /// slices and stacks parameters control the degree of tessellation.
-    ///</summary>
+    /**
+     * \brief Struct which describes single vertex. Creates a sphere centered at the origin with the given radius.  The
+     * slices and stacks parameters control the degree of tessellation.
+     */
     MeshData CreateSphere(float radius, uint32 sliceCount, uint32 stackCount);
-
-    ///<summary>
-    /// Creates a geosphere centered at the origin with the given radius.  The
-    /// depth controls the level of tessellation.
-    ///</summary>
+    /**
+     * \brief Creates a geosphere centered at the origin with the given radius.  The
+     * depth controls the level of tessellation.
+     */
     MeshData CreateGeosphere(float radius, uint32 numSubdivisions);
-
-    ///<summary>
-    /// Creates a cylinder parallel to the y-axis, and centered about the origin.  
-    /// The bottom and top radius can vary to form various cone shapes rather than true
-    // cylinders.  The slices and stacks parameters control the degree of tessellation.
-    ///</summary>
+    /**
+     * \brief Creates a cylinder parallel to the y-axis, and centered about the origin.  
+     * The bottom and top radius can vary to form various cone shapes rather than true
+     * cylinders.  The slices and stacks parameters control the degree of tessellation.
+     */
     MeshData CreateCylinder(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount);
-
-    ///<summary>
-    /// Creates an mxn grid in the xz-plane with m rows and n columns, centered
-    /// at the origin with the specified width and depth.
-    ///</summary>
+    /**
+     * \brief Creates an mxn grid in the xz-plane with m rows and n columns, centered
+     * at the origin with the specified width and depth.
+     */
     MeshData CreateGrid(float width, float depth, uint32 m, uint32 n);
-
-    ///<summary>
-    /// Creates a quad aligned with the screen.  This is useful for postprocessing and screen effects.
-    ///</summary>
+    /**
+     * \brief Creates a quad aligned with the screen.  This is useful for postprocessing and screen effects.
+     */
     MeshData CreateQuad(float x, float y, float w, float h, float depth);
 
 private:
@@ -90,5 +94,4 @@ private:
     Vertex MidPoint(const Vertex& v0, const Vertex& v1);
     void BuildCylinderTopCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData& meshData);
     void BuildCylinderBottomCap(float bottomRadius, float topRadius, float height, uint32 sliceCount, uint32 stackCount, MeshData& meshData);
-
 };
