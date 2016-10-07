e97669a8494cb21fa58874e648d52356fbdb9c71
Alex Vorontsov <nameless323@gmail.com>
2016-10-07 12:15:28 +0400

    RenderTarget comments

--- Source/Common/RenderTarget.h
+++ Source/Common/RenderTarget.h
@@ -1,3 +1,7 @@
+//
+// Describes render target.
+//
+
 #pragma once
 
 #include "../../Core/D3DUtil.h"
@@ -5,16 +9,39 @@
 class RenderTarget
 {
 public:
+    /**
+     * \brief Create render target resource.
+     * \param Device which will be used for creation.
+     * \param Width texture width.
+     * \param Height texture height.
+     * \format Render target texture format.
+     */
     RenderTarget(ID3D12Device* device, UINT width, UINT height, DXGI_FORMAT format);
     RenderTarget(const RenderTarget& rhs) = delete;
     RenderTarget& operator= (const RenderTarget& rhs) = delete;
     ~RenderTarget() = default;
-
+    /**
+     * \brief Get render target resource.
+     */
     ID3D12Resource* Resource();
+    /**
+     * \brief Get render target shader resource view.
+     */
     CD3DX12_GPU_DESCRIPTOR_HANDLE Srv();
+    /**
+     * \brief Get render target view.
+     */
     CD3DX12_CPU_DESCRIPTOR_HANDLE Rtv();
-
+    /**
+     * \brief Build descriptors for render target.
+     * \hCpuSrv Cpu descriptor for render target in SRV heap.
+     * \hGpuSrv Gpu descriptor for render target in SRV heap.
+     * \hCpuRtvCpu descriptor for render target in SRV RTV.
+     */
     void BuildDescriptors(CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuSrv, CD3DX12_GPU_DESCRIPTOR_HANDLE hGpuSrv, CD3DX12_CPU_DESCRIPTOR_HANDLE hCpuRtv);
+    /**
+     * \brief Call when application window are resized. Rebuilds resource according to new width and height.
+     */
     void OnResize(UINT newWidth, UINT newHeight);
 private:
     void BuildDescriptors();
