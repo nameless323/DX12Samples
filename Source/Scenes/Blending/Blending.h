d92bf45d5ede003c6ac0d284ac1200ce032140f7
Alex Vorontsov <nameless323@gmail.com>
2016-10-07 13:49:14 +0400

    Update Blending.h

--- Source/Scenes/Blending/Blending.h
+++ Source/Scenes/Blending/Blending.h
@@ -1,3 +1,7 @@
+//
+// Scene with alpha blending.
+//
+
 #pragma once
 
 #include "../../../Core/Application.h"
@@ -14,42 +18,122 @@ public:
     Blending(const Blending& rhs) = delete;
     Blending& operator=(const Blending& rhs) = delete;
     ~Blending() override;
-
+    /**
+     * \brief Scene initiallization including texture loading, loading all pipline etc.
+     */
     bool Init() override;
     LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
     int Run() override;
 protected:
+    /**
+     * \brief Calls when window are resized to rebuild size dependent resources.
+     */
     void OnResize() override;
+    /**
+     * \brief Update game logic.
+     */
     void Update(const GameTimer& timer) override;
+    /**
+     * \brief Draw scene.
+     */
     void Draw(const GameTimer& timer) override;
-
+    /**
+     * \brief Calls when mouse button down.
+     */
     void OnMouseDown(WPARAM btnState, int x, int y) override;
+    /**
+     * \brief Calls when mouse button up.
+     */
     void OnMouseUp(WPARAM btnState, int x, int y) override;
+    /**
+     * \brief Calls when mouse moves.
+     */
     void OnMouseMove(WPARAM btnState, int x, int y) override;
-
+    /**
+     * \brief Handle keyboard input.
+     */
     void OnKeyboardInput(const GameTimer& timer);
+    /**
+     * \brief Move camera.
+     */
     void UpdateCamera(const GameTimer& timer);
+    /**
+     * \brief Animate materials e.g water.
+     */
     void AnimateMaterials(const GameTimer& timer);
+    /**
+     * \brief Update objects constant buffers for current frame.
+     */
     void UpdateObjectCBs(const GameTimer& timer);
+    /**
+     * \brief Update materials constant buffers for current frame.
+     */
     void UpdateMaterialCBs(const GameTimer& timer);
+    /**
+     * \brief Update pass buffer.
+     */
     void UpdateMainPassCB(const GameTimer& timer);
+    /**
+     * \brief Move waves and update them vertex buffer.
+     */
     void UpdateWaves(const GameTimer& timer);
-
+    /**
+     * \brief Load scene texutres from dds.
+     */
     void LoadTextures();
+    /**
+     * \brief Build scene main root signature.
+     */
     void BuildRootSignature();
+    /**
+     * \brief Build nesessary deccriptor heaps for scene.
+     */
     void BuildDescriptorHeaps();
+    /**
+     * \brief Load shaders from hlsl files and construct input layouts.
+     */
     void BuildShaderAndInputLayout();
+    /**
+     * \brief Build mesh for land.
+     */
     void BuildLandGeometry();
+    /**
+     * \brief Build mesh for water.
+     */
     void BuildWavesGeometry();
+    /**
+     * \brief Build mesh for box.
+     */
     void BuildBoxGeometry();
+    /**
+     * \brief Build pipline state objects.
+     */
     void BuildPSOs();
+    /**
+     * \brief Build nessesary amount for frame resources.
+     */
     void BuildFrameResources();
+    /**
+     * \brief Build materials for scene.
+     */
     void BuildMaterials();
+    /**
+     * \brief Build scene objects.
+     */
     void BuildRenderItems();
+    /**
+     * \brief Draw scene objects.
+     */
     void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& renderItems);
-
+    /**
+     * \brief Height of hills at point x, z.
+     */
     float GetHillsHeight(float x, float z) const;
+    /**
+     * \brief Normalsof hills at point x, z.
+     */
     DirectX::XMFLOAT3 GetHillsNormal(float x, float z) const;
+    
 private:
     std::vector<std::unique_ptr<FrameResourceBlending>> _frameResources;
     FrameResourceBlending* _currFrameResource = nullptr;
