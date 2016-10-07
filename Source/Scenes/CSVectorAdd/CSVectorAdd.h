ae7c540af6f691efef946f2b5a4b3bd5ee7111c7
Alex Vorontsov <nameless323@gmail.com>
2016-10-07 16:27:55 +0400

    comments in VectorAdd

--- Source/Scenes/CSVectorAdd/CSVectorAdd.h
+++ Source/Scenes/CSVectorAdd/CSVectorAdd.h
@@ -1,8 +1,11 @@
+//
+// Bunch of vector addition using compute shaders.
+//
+
 #pragma once
 
 #include "../../../Core/Application.h"
 #include "../../../Core/D3DUtil.h"
-
 
 class CSVectorAdd : public Application
 {
@@ -11,19 +14,44 @@ public:
     CSVectorAdd(const CSVectorAdd& rhs) = delete;
     CSVectorAdd& operator=(const CSVectorAdd& rhs) = delete;
     ~CSVectorAdd() override;
-
+    /**
+     * \brief Scene initiallization including texture loading, loading all pipline etc.
+     */
     bool Init() override;
     LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
     int Run() override;
 protected:
+    /**
+     * \brief Calls when window are resized to rebuild size dependent resources.
+     */
     void OnResize() override;
+    /**
+     * \brief Update game logic.
+     */
     void Update(const GameTimer& timer) override;
+    /**
+     * \brief Draw scene.
+     */
     void Draw(const GameTimer& timer) override;
-
+    /**
+     * \brief Execute computations.
+     */
     void DoComputeWork();
+    /**
+     * \brief Build buffers for computaton data.
+     */
     void BuildBuffers();
+    /**
+     * \brief Build scene main root signature.
+     */
     void BuildRootSignature();
+    /**
+     * \brief Load shaders from hlsl files and construct input layouts.
+     */
     void BuildShaderAndInputLayout();
+    /**
+     * \brief Build pipline state objects.
+     */
     void BuildPSOs();
 private:
     Microsoft::WRL::ComPtr<ID3D12RootSignature> _rootSignature = nullptr;
