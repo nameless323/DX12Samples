84cac8cb86e30f97d22ab44d6a88b73660ded4ae
Alex Vorontsov <nameless323@gmail.com>
2016-10-07 18:13:06 +0400

    comments in crate.h

--- Source/Scenes/Crate/Crate.h
+++ Source/Scenes/Crate/Crate.h
@@ -1,6 +1,9 @@
-#pragma once
+//
+// Simple scene with crate.
+//
 
 #pragma once
+
 #include "../../../Core/Application.h"
 #include "../../../Core/D3DUtil.h"
 
@@ -13,35 +16,101 @@ public:
     Crate(HINSTANCE hInstance);
     Crate(const Crate& rhs) = delete;
     Crate& operator=(const Crate& rhs) = delete;
+    /**
+     * \brief Scene initiallization including texture loading, loading all pipline etc.
+     */
     bool Init() override;
     ~Crate() override;
     LRESULT MsgProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) override;
     int Run() override;
 protected:
+    /**
+     * \brief Calls when window are resized to rebuild size dependent resources.
+     */
     void OnResize() override;
+    /**
+     * \brief Update game logic.
+     */
     void Update(const GameTimer& timer) override;
+    /**
+     * \brief Draw scene.
+     */
     void Draw(const GameTimer& timer) override;
-
+    /**
+     * \brief Calls when mouse button down.
+     */
     void OnMouseDown(WPARAM btnState, int x, int y) override;
+    /**
+     * \brief Calls when mouse button up.
+     */
     void OnMouseUp(WPARAM btnState, int x, int y) override;
+    /**
+     * \brief Calls when mouse moves.
+     */
     void OnMouseMove(WPARAM btnState, int x, int y) override;
-
+    /**
+     * \brief Handle keyboard input.
+     */
     void OnKeyboardInput(const GameTimer& timer);
+    /**
+     * \brief Move camera.
+     */
     void UpdateCamera(const GameTimer& timer);
+    /**
+     * \brief Animate materials e.g water.
+     */
     void AnimateMaterials(const GameTimer& timer);
+    /**
+     * \brief Update objects constant buffers for current frame.
+     */
     void UpdateObjectCBs(const GameTimer& timer);
+    /**
+     * \brief Update materials constant buffers for current frame.
+     */
     void UpdateMaterialsCBs(const GameTimer& timer);
+    /**
+     * \brief Update pass buffer.
+     */
     void UpdateMainPassCB(const GameTimer& timer);
-
+    /**
+     * \brief Load scene texutres from dds.
+     */
     void LoadTextures();
+    /**
+     * \brief Build scene main root signature.
+     */
     void BuildRootSignature();
+    /**
+     * \brief Build nesessary deccriptor heaps for scene.
+     */
     void BuildDescriptorHeaps();
+    /**
+     * \brief Load shaders from hlsl files and construct input layouts.
+     */
     void BuildShaderAndInputLayout();
+    /**
+     * \brief Build crate.
+     */
     void BuildShapeGeometry();
+    /**
+     * \brief Build pipline state objects.
+     */
     void BuildPSOs();
+    /**
+     * \brief Build nessesary amount for frame resources.
+     */
     void BuildFrameResources();
+    /**
+     * \brief Build materials for scene.
+     */
     void BuildMaterials();
+    /**
+     * \brief Build scene objects.
+     */
     void BuildRenderItems();
+    /**
+     * \brief Draw scene objects.
+     */
     void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<CrateRenderItem*>& renderItems);
 
 private:
@@ -73,9 +142,6 @@ private:
     float _theta = 1.3f * DirectX::XM_PI;
     float _phi = 0.4f * DirectX::XM_PI;
     float _radius = 2.5f;
-
     float _rotationAngle = 0.0f;
-
     POINT _lastMousePos;
 };
-
